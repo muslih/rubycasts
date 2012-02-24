96062979f12e4456581eafb96779c0a1cd309805
James Gifford <james@jamesrgifford.com>
2012-02-24 17:25:22 -0500

    Release episode 4

--- episode4/main.rb
+++ episode4/main.rb
@@ -1,13 +1,12 @@
-require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  current_time = "The current time is: <%= Time.now %>."
+  current_time = "The current time is <%= Time.now %>."
   erb current_time
 end
 
 not_found do
-  not_found_page = "Unfortunately, we were unable to find this page as of <%= Time.now %>, <em>please</em> try again in a moment."
+  not_found_page = "Unfortunately, we were unable to find this page as of <b><%= Time.now %></b>."
   erb not_found_page
 end
 
