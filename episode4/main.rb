2a35815166f58168c9b9ac70a68bcd07e74de38e
James Gifford <james@jamesrgifford.com>
2012-02-24 16:00:53 -0500

    Ok, lets rock with this thing

--- episode4/main.rb
+++ episode4/main.rb
@@ -1,11 +1,13 @@
+require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  current_time = "The current time is <%= Time.now %>"
+  current_time = "The current time is: <%= Time.now %>."
   erb current_time
 end
 
 not_found do
-  not_found_page = "<h1>Unfortunately, we were unable to find this page as of <%= Time.now %>, <em>please</em> try again.</h1>"
+  not_found_page = "Unfortunately, we were unable to find this page as of <%= Time.now %>, <em>please</em> try again in a moment."
   erb not_found_page
 end
+
